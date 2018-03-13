d6a0a516237a3f952e10640e36056827bcb35495
henrisli <henrisli@stud.ntnu.no>
2018-03-13 15:18:18 +0100

    adsf

--- Run.py
+++ Run.py
@@ -98,6 +98,8 @@ def Advection_high_resolution_schemes():
 # Solution of Buckley-Leverett equation, both with classical
 # and high-resolution schemes.
 def BL_solution(method, initial, M = 1):
+    def analytical(u):
+        return np.divide(2*M*np.multiply(u,1-u),np.power(np.power(u,2) + np.power(1-u,2),2))
     # Flux function
     def f(u):
         return np.divide(np.power(u,2),np.power(u,2) + M*np.power(1-u,2))
@@ -110,7 +112,7 @@ def BL_solution(method, initial, M = 1):
     df = lambda u: np.zeros(len(u)) + dfv
     
     # Reference solution
-    dx = 1/1000
+    dx = 1/2**12
     xr = np.arange(-0.5*dx,1+1.5*dx,dx)
     # Discontinuous initial:
     if initial == 'dis':
@@ -121,7 +123,7 @@ def BL_solution(method, initial, M = 1):
         u0 = np.sin((xr-0.1)*np.pi/0.3)**2
         u0[(xr<0.1) | (xr>0.4)] = 0
         
-    ur = upw(u0, 0.995, dx, 0.65, f, df, outflow)
+    ur = upw(u0, 0.995, dx, 0.5, f, df, outflow)
     
      # Solutions on coarser grids
     N  = 50
@@ -139,23 +141,35 @@ def BL_solution(method, initial, M = 1):
             u0 = np.sin((x-0.1)*np.pi/0.3)**2
             u0[(x<0.1) | (x>0.4)] = 0
         
-        uu = upw(u0, .995, dx, 0.65, f, df, outflow)
-        uf = lxf(u0, .995, dx, 0.65, f, df, outflow)
-        uw = lxw(u0, .995, dx, 0.65, f, df, outflow)
-    
+        uu = upw(u0, .995, dx, .5, f, df, outflow)
+        uf = lxf(u0, .995, dx, .5, f, df, outflow)
+        uw = lxw(u0, .995, dx, .5, f, df, outflow)
+        
+        
+        xref = np.linspace(1/np.sqrt(2),1,1001,endpoint = True)
+        ua = analytical(xref)/2
         # Plot results
         plt.figure()
         plt.plot([1,2,3])
         plt.subplot(131)
-        plt.plot(xr[1:-1], ur[1:-1])
+        plt.plot(xr[1:-1],ur[1:-1])
+        #plt.plot(ua,xref, color = 'red')
+        #plt.axvline(x = 1/4 + 1/np.sqrt(8), color = 'red', ymin = 0.045, ymax =0.687)
+        #plt.axhline(y = 0, xmin = 1/4 + 1/np.sqrt(8), xmax = 0.98, color = 'red')
         plt.plot(x[1:-1], uu[1:-1], '.', markersize = 3)
         plt.title("Upwind")
         plt.subplot(132)
         plt.plot(xr[1:-1],ur[1:-1])
+        #plt.plot(ua,xref, color = 'red')
+        #plt.axvline(x = 1/4 + 1/np.sqrt(8), color = 'red', ymin = 0.045, ymax =0.687)
+        #plt.axhline(y = 0, xmin = 1/4 + 1/np.sqrt(8), xmax = 0.98, color = 'red')
         plt.plot(x[1:-1],uf[1:-1],'.', markersize = 3)
         plt.title("Lax-Friedrichs")
         plt.subplot(133)
         plt.plot(xr[1:-1],ur[1:-1])
+        #plt.plot(ua,xref, color = 'red')
+        #plt.axvline(x = 1/4 + 1/np.sqrt(8), color = 'red', ymin = 0.045, ymax =0.687)
+        #plt.axhline(y = 0, xmin = 1/4 + 1/np.sqrt(8), xmax = 0.98, color = 'red')
         plt.plot(x[1:-1],uw[1:-1],'.', markersize = 3)
         plt.title("Lax-Wendroff")
         if initial == 'cont':
@@ -197,27 +211,18 @@ def BL_solution(method, initial, M = 1):
             plt.savefig("solution_high_discont.pdf")
 
 
-def Error_verification_space(method, initial):
+def Error_verification(method, initial):
     # BL flux function:
     def f(u):
         return np.divide(np.power(u,2),np.power(u,2) + np.power(1-u,2))
-    
-    # Advection flux:
-    #def f(u):
-    #    return u
-    
     # Derivative of flux for BL:
     s = np.linspace(0,1,501)
     dfv = max(np.diff(f(s))/np.diff(s))
     df = lambda u: np.zeros(len(u)) + dfv
     
-    # Derivative of flux for advection:
-    #df = lambda u: np.zeros(len(u)) + 1
-        
     # Reference solution
-    dx = 1/1000
+    dx = 1/2**12
     xr = np.arange(-0.5*dx,1+1.5*dx,dx)
-    print(xr)
     # Discontinuous initial:
     if initial == 'dis':
         u0 = np.zeros(len(xr))
@@ -228,10 +233,10 @@ def Error_verification_space(method, initial):
         u0 = np.sin((xr-0.1)*np.pi/0.3)**2
         u0[(xr<0.1) | (xr>0.4)] = 0
     
-    ur = upw(u0, .995, dx, .65, f, df, outflow)
+    ur = upw(u0, .995, dx, 1, f, df, outflow)
     
     # Solutions on coarser grids
-    N  = np.array([5,10,20,80,160])
+    N  = np.array([2**i for i in range(1,9)])
     
     if method == 'classical':
         error_upw = np.zeros(len(N))
@@ -245,7 +250,6 @@ def Error_verification_space(method, initial):
         dX = 1/n
         if method == 'classical':
             x  = np.arange(-0.5*dX,1+1.5*dX,dX)
-            print(x)
             # Discontinuous initial:
             if initial == 'dis':
                 u0 = np.zeros(len(x))
@@ -254,10 +258,10 @@ def Error_verification_space(method, initial):
             elif initial == 'cont':
                 u0 = np.sin((x-0.1)*np.pi/0.3)**2
                 u0[(x<0.1) | (x>0.4)] = 0
-            uu = upw(u0, .995, dX, .65, f, df, outflow)
-            uf = lxf(u0, .995, dX, .65, f, df, outflow)
-            uw = lxw(u0, .995, dX, .65, f, df, outflow)
-            x_e = [False]*1001
+            uu = upw(u0, .995, dX, 1, f, df, outflow)
+            uf = lxf(u0, .995, dX, 1, f, df, outflow)
+            uw = lxw(u0, .995, dX, 1, f, df, outflow)
+            x_e = [False]*(2**12+1)
             for i in range(len(x[1:-1])):
                 x_e[int(dX/dx*(i+0.5))] = True
             ur_e = 0.5*ur[1:] + 0.5*ur[:-1]
@@ -286,9 +290,9 @@ def Error_verification_space(method, initial):
                 u0 = np.sin((xh-0.1)*np.pi/0.3)**2
                 u0[(xh<0.1) | (xh>0.4)] = 0
                 
-            un = nt(u0, .495, dX, .65, f, df, outflow)
-            uc = cuw(u0, .495, dX, .65, f, df, outflow)
-            x_e = [False]*4001
+            un = nt(u0, .495, dX, 1, f, df, outflow)
+            uc = cuw(u0, .495, dX, 1, f, df, outflow)
+            x_e = [False]*(2**12+1)
             for i in range(len(xh[2:-2])):
                 x_e[int(dX/dx*(i+0.5))] = True
             ur_e = 0.5*ur[1:] + 0.5*ur[:-1]
@@ -304,7 +308,6 @@ def Error_verification_space(method, initial):
             error_cuw[j] = np.sqrt(dX)*np.linalg.norm(uc - ur_comp, 2)
             j += 1
     
-    
     if method == 'classical':
         plt.figure()
         plt.loglog([1/i for i in N],error_upw)
@@ -312,21 +315,24 @@ def Error_verification_space(method, initial):
         plt.loglog([1/i for i in N],error_lxw)
         plt.legend(["UW","LF","LW"])
         plt.ylabel("Error")
-        plt.xlabel("N")
+        plt.xlabel("dx")
         if initial=='dis':
             plt.savefig("Error_classical_disc.pdf")
         elif initial=='cont':
             plt.savefig("Error_classical_cont.pdf")
     elif method == 'high':
         plt.figure()
-        plt.loglog(N,error_nt)
-        plt.loglog(N,error_cuw)
+        plt.loglog([1/i for i in N],error_nt)
+        plt.loglog([1/i for i in N],error_cuw)
         plt.legend(["NT","CUW"])
         plt.ylabel("Error")
-        plt.xlabel("N")
+        plt.xlabel("dx")
         if initial=='dis':
             plt.savefig("Error_high_disc.pdf")
         elif initial=='cont':
             plt.savefig("Error_high_cont.pdf")
-
-Error_verification_space('classical','dis')
+            
+            
+            
+Error_verification('classical','dis')
+#BL_solution('classical','dis')
